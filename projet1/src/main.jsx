3cabe0bb746bc7e0b63e6243556dcc4fce354364
Dabadie Axel <axel.dabadie.pro@gmail.com>
2024-11-21 12:25:30 +0100

    version 3

--- projet1/src/main.jsx
+++ projet1/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import barrenav from './pages/barrenav/navebar.jsx'
 import './index.css';
 
 import { createBrowserRouter, RouterProvider, useNavigate } from 'react-router-dom';
